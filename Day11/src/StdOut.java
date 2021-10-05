9fbed20a64688b18099bad1a1e02f586ac64043a
shivammathur004 <shivamm.4aug@gmail.com>
2021-10-05 09:22:56 +0530

    [Shivam] Add : Day11 Java Programs

---
+++ Day11/src/StdOut.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.css.Counter;
+
+public interface StdOut {
+    static void println(Counter zipf) {
+    }
+
+    static void printf(String string, int uniform) {
+    }
+
+    static void printf(String string, double gaussian) {
+    }
+
+    static void printf(String string, boolean bernoulli) {
+    }
+
+    static void printf(String string, double sum1, double time1) {
+    }
+
+    static void println(String string) {
+    }
+
+    static void print(String string) {
+    }
+
+    static void println() {
+    }
+
+
+}
---
+++ Day11/src/StopWatch.java
@@ -0,0 +1,36 @@
+public class StopWatch {
+    public class Stopwatch {
+
+        private final long start;
+
+        public Stopwatch() {
+            start = System.currentTimeMillis();
+        }
+
+        public double elapsedTime() {
+            long now = System.currentTimeMillis();
+            return (now - start) / 1000.0;
+        }
+
+        public void main(String[] args) {
+            int n = Integer.parseInt(args[0]);
+
+
+            Stopwatch timer1 = new Stopwatch();
+            double sum1 = 0.0;
+            for (int i = 1; i <= n; i++) {
+                sum1 += Math.sqrt(i);
+            }
+            double time1 = timer1.elapsedTime();
+            StdOut.printf("%e (%.2f seconds)\n", sum1, time1);
+
+            Stopwatch timer2 = new Stopwatch();
+            double sum2 = 0.0;
+            for (int i = 1; i <= n; i++) {
+                sum2 += Math.pow(i, 0.5);
+            }
+            double time2 = timer2.elapsedTime();
+            StdOut.printf("%e (%.2f seconds)\n", sum2, time2);
+        }
+    }
+}
